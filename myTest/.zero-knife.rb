5d9f79b697e9e6faed127a0d942a78bf1c7c467a
Dan Griffis <dgriffis@glgroup.com>
2013-08-30 16:03:41 -0400

    Initial checkin

---
+++ myTest/.zero-knife.rb
@@ -0,0 +1,3 @@
+            chef_server_url 'http://10.115.100.15:4000'
+            node_name 'zero-host'
+            client_key '/var/folders/jm/kk78s8zd1q9cqr36_bsqgbhh6k_8x3/T/fake_key/fake.pem'
---
+++ myTest/metadata.rb
@@ -0,0 +1,9 @@
+name             'playground'
+maintainer       'Dan Griffis'
+maintainer_email 'dgriffis@glgroup.com'
+license          'Apache 2.0'
+description      'Installs/Configures myface'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
+depends "apache2", "~> 1.6.0"
